6559e8cfe1e9005054b296c85d3ca2adc5d0c0d0
root <root@MAY.localdomain>
2025-11-17 18:45:33 +0100

    Added a new path to the class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.views.generic import DetailView
 
 
@@ -16,7 +17,7 @@ def list_books(request):
 # Class-Based View: Display library details and its books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
 # Create your views here.
