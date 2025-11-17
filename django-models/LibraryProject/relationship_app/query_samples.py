eda53fcb254d7d3aee6c5c744ac8d39dde980d31
root <root@MAY.localdomain>
2025-11-17 17:06:39 +0100

    Changed the name specification

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query 1: All books by a specific author
-author = Author.objects.get(name="George Orwell")
+author = Author.objects.get(name="Author_name")
 books_by_author = author.books.all()
 print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
 
 
 # Query 2: List all books in a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name="Library_name")
 books_in_library = library.books.all()
 print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
 
