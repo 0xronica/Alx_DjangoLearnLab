dec6c769c1ddd533c37831c702a6687f10c33138
root <root@MAY.localdomain>
2025-11-22 20:31:51 +0100

    Utilized @user_passes_test for each view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,4 +37,36 @@ def Logoutview(request):
     logout(request)
     return render(request, "relationship_app/logout.html")
 
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Helper functions for access control
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+# ADMIN VIEW
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+# LIBRARIAN VIEW
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+# MEMBER VIEW
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 # Create your views here.
