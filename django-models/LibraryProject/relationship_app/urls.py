c73a664779f9703fd67488c04e9f7562925b5e68
root <root@MAY.localdomain>
2025-11-22 21:41:37 +0100

    Added url patterns for the add, edit and delete views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,3 +18,24 @@ urlpatterns = [
 
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
+
+from django.urls import path
+from .views  import admin_view
+from .views import librarian_view
+from .views import member_view
+
+urlpatterns = [
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+]
+
