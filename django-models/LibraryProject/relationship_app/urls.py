aa48c85ff5fc46e7b7c3f5cbc1375ad9a1341602
root <root@MAY.localdomain>
2025-11-22 19:20:43 +0100

    Restructured the line of code for login view's url pattern

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,9 +14,7 @@ from .views import RegisterView
 urlpatterns = [
     path('register/', views.register, name='register'),
 
-    path('login/', LoginView.as_view(
-        template_name='relationship_app/login.html'
-    ), name='login'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
