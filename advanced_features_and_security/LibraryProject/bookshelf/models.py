c37dad9ed04aa19cf6570b63e080200a32b137af
root <root@MAY.localdomain>
2025-11-23 18:32:43 +0100

    Created an Article model with meta class permissions

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -6,6 +6,21 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view articles"),
+            ("can_create", "Can create articles"),
+            ("can_edit", "Can edit articles"),
+            ("can_delete", "Can delete articles"),
+        ]
+
+ def __str__(self):
+        return self.title
+
 # accounts/models.py
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
