a7f9131ba3d4800386d999ba2374c2f859c2e765
root <root@MAY.localdomain>
2025-11-30 23:33:30 +0100

    Incorporate filtering, searchin g, and ordering functionalities

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
