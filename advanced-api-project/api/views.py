5565cd32f0ff9fc8cb7b35114fc50ec875721226
root <root@MAY.localdomain>
2025-11-30 23:08:18 +0100

    Added permission import fucntio n

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions  import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
