ef0c2e4d682a3e90a74f73fe68d8e8ec107269c8
root <root@MAY.localdomain>
2025-12-01 00:15:36 +0100

    Defined the path for CRUD router operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
 
+# Create a router and register the BookViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
 ]
-
-# Create a router and register the BookViewSet
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
 
     
