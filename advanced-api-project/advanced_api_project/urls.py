e3df7e9e8acb37ab8184ef247a6e5c7ec6eeb497
root <root@MAY.localdomain>
2025-11-30 22:52:47 +0100

    Define the url paths for each views

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import (
+    BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView
+)
 
+
+urlpatterns = [
+    path("books/", ListView.as_view(), name="book-list"),
+    path("books/<int:pk>/", DetailView.as_view(), name="book-detail"),
+    path("books/create/", CreateView.as_view(), name="book-create"),
+    path("books/<int:pk>/update/", UpdateView.as_view(), name="book-update"),
+    path("books/<int:pk>/delete/", DeleteView.as_view(), name="book-delete"),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
